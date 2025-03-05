564b37fa48e907e9c21dec7ac4a5659d070aa345
marat <maratakhun@gmail.com>
2025-03-05 17:53:37 +0300

    1

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,6 +7,16 @@ app = Flask(__name__)
 def index():
     title = "Подготовка к миссии колонизации Марса"
     return render_template('base.html', title=title)
+
+@app.route('/training/<prof>')
+def training(prof):
+    if 'инженер' in prof.lower() or 'строитель' in prof.lower():
+        header = "Инженерные тренажеры"
+        image_url = url_for('static', filename='image-987.jpg')
+    else:
+        header = "Научные симуляторы"
+        image_url = url_for('static', filename='image-987.jpg')
+    return render_template('training.html', header=header, image_url=image_url)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
